f15260f34a19ee7fbd8db522caf6b74838fdf809
setiawansandi <81544477+setiawansandi@users.noreply.github.com>
2024-10-18 01:32:57 +0800

    add locations table

--- src/migrations/createTables.ts
+++ src/migrations/createTables.ts
@@ -1,10 +1,13 @@
 import pool from '../config/db'; // Database connection
 import { createUsersTable } from '../tables/usersTable'; // Import the createUsersTable function
+import { createLocationsTable, insertLocations } from '../tables/locationsTable';
 
 // Main function to create all tables
 const createAllTables = async () => {
     try {
         await createUsersTable();  // Create the users table
+        await createLocationsTable(); // Create the locations table
+        await insertLocations(); // Insert placeholder data
     } catch (error) {
         console.error('Error creating tables:', error);
     } finally {
