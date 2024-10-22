6edbd7513d7fb1c96f89d55e0a1865b14fd88f00
yannitetsuyu <yanni.ler@tetsuyuhealthcare.com>
2024-10-22 12:05:32 +0800

    expenses table

--- src/migrations/createTables.ts
+++ src/migrations/createTables.ts
@@ -1,6 +1,7 @@
 import pool from '../config/db'; // Database connection
 import { createUsersTable } from '../tables/usersTable'; // Import the createUsersTable function
 import { createLocationsTable, insertLocations } from '../tables/locationsTable';
+import { createExpensesTable } from '../tables/expensesTable';
 
 // Main function to create all tables
 const createAllTables = async () => {
@@ -8,6 +9,7 @@ const createAllTables = async () => {
         await createUsersTable();  // Create the users table
         await createLocationsTable(); // Create the locations table
         await insertLocations(); // Insert placeholder data
+        await createExpensesTable(); // Create Expenses table
     } catch (error) {
         console.error('Error creating tables:', error);
     } finally {
